cb1e38efb97d0580e1eeefda6e8a5b254c92ce4e
Leto <mikhaylichenko2000@bk.ru>
2022-08-26 14:44:04 +0300

    Выполнил дополнительную задачу №42

---
+++ HW42+/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+// ДОП.ЗАДАЧА:
+// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+//Пришлось изучить коллекции
+List<int> resultList = new List<int>();
+while (N > 0)
+{
+    result = N % 2;
+    N = N / 2;
+    resultList.Add(result);
+}
+resultList.Reverse();
+
+foreach (int i in resultList)
+{
+    Console.Write($"{i}");
+}
+
